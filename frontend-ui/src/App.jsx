483f781dab1a499d9de56108fb4302c35d5e47ee
Ayush Patel <104913698+Ayush200423@users.noreply.github.com>
2024-05-16 18:33:45 -0700

    React app and font setup

---
+++ frontend-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/fonts/Poppins/Poppins-Medium.ttf";
+import "./index.css";
+import Popover from "./components/popover.jsx";
+
+const App = () => {
+  return (
+    <div className="text-[30px] font-poppinsBold">
+      <Popover />
+    </div>
+  );
+};
+
+export default App;
